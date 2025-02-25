c8f76139f8c46cd1a3a6edf22bc808f99f7008fc
pankaj kumar <pankajamsali@gmail.com>
2025-02-25 12:11:34 +0530

    update axios instance baseURL for development and production environments

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
-import axios from 'axios';
+import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'https://chatflow-nine.vercel.app/api'
-      : 'https://chatflow-nine.vercel.app/api',
-  withCredentials: true, //Enables sending and receiving cookies or other credentials with cross-origin requests.
-});
+    baseURL:import.meta.env.MODE ==="development" ? "http://localhost:8000/api" : "/api",
+    withCredentials:true //Enables sending and receiving cookies or other credentials with cross-origin requests.
+})
