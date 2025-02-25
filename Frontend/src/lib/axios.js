8b63ae2b046759df18dcfaf866692210f0e1511a
pankaj kumar <pankajamsali@gmail.com>
2025-02-25 11:55:32 +0530

    update socket and axios configurations for production deployment

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from "axios"
+import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE ==="development" ? "http://localhost:8000/api" : "/api",
-    withCredentials:true //Enables sending and receiving cookies or other credentials with cross-origin requests.
-})
+  baseURL:
+    import.meta.env.MODE === 'development'
+      ? 'https://chatflow-git-main-pankaj-kumars-projects-8ff53621.vercel.app/api'
+      : '/api',
+  withCredentials: true, //Enables sending and receiving cookies or other credentials with cross-origin requests.
+});
